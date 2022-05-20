7df49b61293c708d6f722a0ab074af3d4a9349fd
Alex-Berezov <aleber197@gmail.com>
2022-05-20 13:32:24 +0400

    New tasks resolved

--- codeWars.js
+++ codeWars.js
@@ -1,5 +1,130 @@
 
 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour
+// and returns it in cm per second, rounded down to the integer (= floored).
+// For example:
+// 1.08 --> 30
+// Note! The input is a Real number (actual type is language dependent) and is >= 0.
+// The result should be an Integer.
+
+function cockroachSpeed(s) {
+  const KM_TO_SM = s * 100000
+  const HOUR_TO_SECOND = 3600
+  return Math.floor(KM_TO_SM / HOUR_TO_SECOND)
+}
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>");
+
+console.log("30 >>", cockroachSpeed(1.08));
+console.log("30 >>", cockroachSpeed(1.09));
+console.log("61 >>", cockroachSpeed(2.1904661083891077));
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>");
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Introduction
+// The first century spans from the year 1 up to and including the year 100,
+// the second century - from the year 101 up to and including the year 200, etc.
+// Task
+// Given a year, return the century it is in.
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+// function century(year) {
+//   return Math.ceil(year / 100)
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number
+// is divisible by both integers a and b.
+// A few cases:
+// (-12, 2, -6)  ->  true
+// (-12, 2, -5)  ->  false
+// (45, 1, 6)    ->  false
+// (45, 5, 15)   ->  true
+// (4, 1, 4)     ->  true
+// (15, -5, 3)   ->  true
+
+// function isDivideBy(number, a, b) {
+//   return number % a === 0 && number % b === 0
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for
+// you and your girlfriend and try to leave all the mess behind you.
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental
+// makes you some good offers.
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total.
+// Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+// Write a code that gives out the total amount for different days(d).
+
+// function rentalCarCost(d) {
+//   const CAR_COST = 40
+//   if (d < 3) return d * CAR_COST
+//   if (d >= 3 && d < 7) return d * CAR_COST - 20
+//   return d * CAR_COST - 50
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// When provided with a number between 0-9, return it in words.
+// Input :: 1 => Output :: "One".
+// If your language supports it, try using a switch statement.
+
+// function switchItUp(number){
+//   const numbers = {
+//     0: 'Zero',
+//     1: 'One',
+//     2: 'Two',
+//     3: 'Three',
+//     4: 'Four',
+//     5: 'Five',
+//     6: 'Six',
+//     7: 'Seven',
+//     8: 'Eight',
+//     9: 'Nine'
+//   }
+//   return numbers[number]
+// }
+
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+// Examples: (Input --> Output)
+// 13 --> "drink toddy"
+// 17 --> "drink coke"
+// 18 --> "drink beer"
+// 20 --> "drink beer"
+// 30 --> "drink whisky"
+
+// function peopleWithAgeDrink(old) {
+//   if (old < 14) return "drink toddy"
+//   if (old >= 14 && old < 18) return "drink coke"
+//   if (old >= 18 && old < 21) return "drink beer"
+//   return "drink whisky"
+// };
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
 // Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals,
 // saying each of the following phrases each time a petal was torn:
 
@@ -14,15 +139,7 @@
 // Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of
 // petals, where nb_petals > 0.
 
-function howMuchILoveYou(nbPetals) {
-  const phrases = [ 'I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all' ];
-  return phrases[(nbPetals - 1) % phrases.length];
-}
-
-console.log(">>>>>>>>>>>>>>>>>>>>>>>>>");
-
-console.log("I love you >>", howMuchILoveYou(7));
-console.log("a lot >>", howMuchILoveYou(3));
-console.log("not at all >>", howMuchILoveYou(6));
-
-console.log(">>>>>>>>>>>>>>>>>>>>>>>>>");
+// function howMuchILoveYou(nbPetals) {
+//   const phrases = [ 'I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all' ];
+//   return phrases[(nbPetals - 1) % phrases.length];
+// }
