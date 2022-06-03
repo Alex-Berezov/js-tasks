6656fbd36772c00d2f42ca1c1bde82e14e1c130a
Alex-Berezov <aleber197@gmail.com>
2022-06-03 22:41:56 +0400

    added new solutions

--- codeWars.js
+++ codeWars.js
@@ -1,22 +1,37 @@
 
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Your friend has been out shopping for puppies (what a time to be alive!)...
+// He arrives back with multiple dogs, and you simply do not know how to respond!
+// By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+// The number of dogs will always be a number and there will always be at least 1 dog.
+
+const howManyDalmatians = (number) => {
+  const dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"]
+  
+  if (number <= 10) return dogs[0]
+  if (number <= 50) return dogs[1]
+  if (number === 101) return dogs[3]
+  return dogs[2]
+}
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>")
+
+console.log("6 >>", mango(3, 3))
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>")
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // There's a "3 for 2" (or "2+1" if you like) offer on mangoes.
 // For a given quantity and price (per mango), calculate the total cost of the mangoes.
 // Examples
 // mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
 // mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
 
-function mango(quantity, price){
-  return quantity / 3 * 2 * price
-}
+// function mango(quantity, price){
+//   return (quantity - Math.floor(quantity / 3)) * price
+// }
 
-console.log(">>>>>>>>>>>>>>>>>>>>>>>>>")
-
-console.log("6 >>", mango(3, 3))
-console.log("30 >>", mango(9, 5))
-
-console.log(">>>>>>>>>>>>>>>>>>>>>>>>>")
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // This is the first step to understanding FizzBuzz.
