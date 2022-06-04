9eec0b4f81c8e9a3a8586684a6eac4f33bd1a204
Alex-Berezov <aleber197@gmail.com>
2022-06-04 10:55:41 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -1,25 +1,7 @@
 
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
-// Your friend has been out shopping for puppies (what a time to be alive!)...
-// He arrives back with multiple dogs, and you simply do not know how to respond!
-// By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
-// The number of dogs will always be a number and there will always be at least 1 dog.
 
-const howManyDalmatians = (number) => {
-  const dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"]
-  
-  if (number <= 10) return dogs[0]
-  if (number <= 50) return dogs[1]
-  if (number === 101) return dogs[3]
-  return dogs[2]
-}
 
-console.log(">>>>>>>>>>>>>>>>>>>>>>>>>")
-
-console.log("6 >>", mango(3, 3))
-
-console.log(">>>>>>>>>>>>>>>>>>>>>>>>>")
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // There's a "3 for 2" (or "2+1" if you like) offer on mangoes.
