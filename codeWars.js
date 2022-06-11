2c54d68e62d876a56db75c95e73f7f6c6328a6f0
Alex-Berezov <aleber197@gmail.com>
2022-06-11 10:59:38 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -1,5 +1,48 @@
 
 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep.
+// Fortunately, you are good at spotting them.
+
+// Warn the sheep in front of the wolf that it is about to be eaten.
+// Remember that you are standing at the front of the queue which is at the end of the array:
+
+// [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+//    7      6      5      4      3            2      1
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep".
+// Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+// Note: there will always be exactly one wolf in the array.
+
+// Examples
+// Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+// Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+// Input: ["sheep", "sheep", "wolf"]
+// Output: "Pls go away and stop eating my sheep"
+
+// const warnTheSheep = (queue) => {
+//   const queueReverse = queue.reverse()
+//   for (let i = 0; i < queueReverse.length; i++) {
+//     if (queueReverse[0] === 'wolf') return "Pls go away and stop eating my sheep"
+//     if (queueReverse[i] === 'wolf') return `Oi! Sheep number ${i}! You are about to be eaten by a wolf!`
+//   }
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+
+// class Ball {
+//   constructor(ballType) {
+//     this.ballType = ballType ? ballType : 'regular'
+//   }
+// }
+
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor.
