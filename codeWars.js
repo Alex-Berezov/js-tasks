30c64c530f6c873006acb02b02d66350dbc61afa
Alex-Berezov <aleber197@gmail.com>
2022-06-30 10:46:56 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -2,6 +2,39 @@
 
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+// const correctPolishLetters = (string) => {
+//   return string
+//     .replace(/ł/g, 'l')
+//     .replace(/ą/g, 'a')
+//     .replace(/ć/g, 'c')
+//     .replace(/ę/g, 'e')
+//     .replace(/ń/g, 'n')
+//     .replace(/ó/g, 'o')
+//     .replace(/ś/g, 's')
+//     .replace(/ź/g, 'z')
+//     .replace(/ż/g, 'z')
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // SpeedCode #2 - Array Madness
 // Objective
 // Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of
