85e2ffe29378f1897e77079fdf7518d1edaa603f
Alex-Berezov <aleber197@gmail.com>
2022-07-05 10:30:38 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -2,6 +2,29 @@
 
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Numbers ending with zeros are boring.
+// They might be fun in your world, but not here.
+// Get rid of them. Only the ending ones.
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
+
+// const noBoringZeros = (n) => {
+//   const arr = [...'' + n].map(String)
+
+//   if (arr[arr.length - 1] !== '0') return n
+
+//   while (arr[arr.length - 1] === '0') {
+//     arr.pop()
+//   }
+
+//   return +arr.join('')
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // You and a friend have decided to play a game to drill your statistical intuitions.
 // The game works like this:
 // You have a bunch of red and blue marbles. To start the game you grab a handful of marbles of each color and
