3be985df1ef4fa8db5ad4efbcc6735f0e34015d5
Alex-Berezov <aleber197@gmail.com>
2022-07-07 10:46:26 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -2,6 +2,16 @@
 
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// *** No Loops Allowed ***
+// You will be given an array (a) and a value (x).
+// All you need to do is check whether the provided array contains the value, without using a loop.
+// Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not.
+// With strings you will need to account for case.
+
+// const check = (a,x) => a.includes(x)
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // Now we will confect a reagent. There are eight materials to choose from, numbered 1,2,..., 8 respectively.
 // We know the rules of confect:
 
