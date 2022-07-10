0b73315a1a8027c490fbccfd09ae14489295aef3
Alex-Berezov <aleber197@gmail.com>
2022-07-10 14:08:34 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -2,6 +2,28 @@
 
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+// You need to consider the following ratings:
+// Terrible: tip 0%
+// Poor: tip 5%
+// Good: tip 10%
+// Great: tip 15%
+// Excellent: tip 20%
+// The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+// "Rating not recognised" in Javascript, Python and Ruby...
+// ...or null in Java
+// ...or -1 in C#
+// Because you're a nice person, you always round up the tip, regardless of the service.
+
+// const calculateTip = (amount, rating) => {
+//   const tipsRule = { terrible: 0, poor: 0.05, good: 0.1, great: 0.15, excellent: 0.2 }
+//   return rating.toLowerCase() in tipsRule
+//     ? Math.ceil(amount * tipsRule[rating.toLowerCase()])
+//     : 'Rating not recognised'
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // You take your son to the forest to see the monkeys. You know that there are a certain number there (n),
 // but your son is too young to just appreciate the full number, he has to start counting them from 1.
 // As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and
