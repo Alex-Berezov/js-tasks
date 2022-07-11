842d3abaa7f5533b0f6af82629f7256515ba35c2
Alex-Berezov <aleber197@gmail.com>
2022-07-11 10:09:17 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -2,6 +2,29 @@
 
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+// For example n = 3 result in:
+
+// "I\n I\n  I"
+// or printed:
+// I
+//  I
+//   I
+// Another example, a 7-step stairs should be drawn like this:
+// I
+//  I
+//   I
+//    I
+//     I
+//      I
+//       I
+
+// const drawStairs = (n) => {
+//   return [...Array(n)].map((_, i) => ' '.repeat(i) + 'I').join('\n')
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
 // You need to consider the following ratings:
 // Terrible: tip 0%
