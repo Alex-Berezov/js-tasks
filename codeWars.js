57ddcb152623f43095731f1cbb0e30ca186fac5d
Alex-Berezov <aleber197@gmail.com>
2022-07-12 09:31:43 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -1,5 +1,15 @@
 
 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Create a function called _if which takes 3 arguments: a boolean value bool and 2 functions (which do not take any parameters):
+// func1 and func2
+// When bool is truth-ish, func1 should be called, otherwise call the func2.
+// Example:
+// _if(true, function(){console.log("True")}, function(){console.log("false")})
+// Logs 'True' to the console.
+
+// const _if = (bool, func1, func2) => bool ? func1() : func2()
+
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
