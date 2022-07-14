2fe52873d21e390c5b7a2271c2363b4f63cdc222
Alex-Berezov <aleber197@gmail.com>
2022-07-14 09:44:31 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -2,6 +2,16 @@
 
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Write a function that always returns 5
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+// Good luck :)
+
+// const unusualFive = () => {
+//   return ['a', 'b', 'c', 'd', 'e'].length
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // Implement String#digit? (in Java StringUtils.isDigit(String)),
 // which should return true if given object is a digit (0-9), false otherwise.
 
