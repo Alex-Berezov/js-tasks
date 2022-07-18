609c0f12099d42a86720bf13ced5a83ef81aff42
Alex-Berezov <aleber197@gmail.com>
2022-07-18 09:33:24 +0400

    Added new solutions

--- codeWars.js
+++ codeWars.js
@@ -2,6 +2,16 @@
 
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+// 2(h × W) + 2(h × L) + 2(W × L)
+
+// const getSize = (width, height, depth) => {
+//   return [(((width * height) * 2) + ((width * depth) * 2) + ((height * depth) * 2)), width * height * depth]
+// }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 // Create a function that finds the integral of the expression passed.
 // In order to find the integral all you need to do is add one to the exponent (the second argument),
 // and divide the coefficient (the first argument) by that new number.
